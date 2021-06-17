c0c4bff98cc26655437affd2c931a07570c7d9b7
Erin Rugas <erinjohnrugas@gmail.com>
2021-06-17 20:57:10 +0800

    include recovery code

---
+++ src/resources/views/users/recovery-code/recovery-code.blade.php
@@ -0,0 +1,13 @@
+<p>
+    <small>
+        Save this recovery code in a secure password manager or your notes. 
+        Use this to access your account in case you lost your phone or the authentication app.
+        <br>
+        <strong>NOTE: Once recovery code is already used. It will be replaced by new one.</strong>
+    </small>
+</p>
+<ul class="list-group list-group text-center mb-3">
+    @foreach (auth()->user()->decryptRecoveryCode() as $recoveryCode)
+        <li class="list-group-item">{{ $recoveryCode }}</li>
+    @endforeach
+</ul>
