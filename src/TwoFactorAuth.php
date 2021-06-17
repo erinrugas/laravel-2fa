e899145d9140cf8ec78094098f96e2b496b2e9b4
Erin Rugas <erinjohnrugas@gmail.com>
2021-06-17 20:57:31 +0800

    added recovery code

--- src/TwoFactorAuth.php
+++ src/TwoFactorAuth.php
@@ -3,8 +3,17 @@
 namespace ErinRugas\Laravel2fa;
 
 use Illuminate\Support\Str;
-
 class TwoFactorAuth
 {
-    
+
+    /**
+     * Generate Recovery Code
+     *
+     * @return string
+     */
+    public function recoveryCode()
+    {
+        return Str::random(6) .'-'. Str::random(6);
+    }
+
 }
