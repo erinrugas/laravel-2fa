648a4da8aea920b7d7b5bce40f55d4483e2d4710
Erin Rugas <erinjohnrugas@gmail.com>
2021-06-21 15:54:05 +0800

    added enhance validate login and added 2fa route

--- src/Http/Requests/Auth/ValidateLogin.php
+++ src/Http/Requests/Auth/ValidateLogin.php
@@ -5,10 +5,12 @@ namespace App\Http\Requests\Auth;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class ValidateLogin extends FormRequest
 {
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -39,12 +41,40 @@ class ValidateLogin extends FormRequest
      */
     public function authenticated()
     {
-        if (! Auth::attempt(['email' => $this->email, 'password' => $this->password], $this->boolean('remember'))) {
+        $model = $this->guard()->getProvider()->getModel();
+
+        $user = $model::where('email', $this->email)->first();
+
+        if (!Hash::check($this->password, $user->password)) {
+
             throw ValidationException::withMessages([
                 'email' => __('auth.failed'),
             ]);
         }
 
-        return redirect(RouteServiceProvider::HOME);
+        if (!is_null($user->two_factor_secret_key) && !is_null($user->two_factor_recovery_code)) {
+            $this->session()->put('user_id', $user->id);
+            $this->session()->put('two_factor_secret', $user->two_factor_secret_key);
+            $this->session()->put('two_factor_recovery', $user->two_factor_recovery_code);
+            $this->session()->put('remember', $this->boolean('remember'));
+
+            return redirect()->route('two-factor-authentication');
+        } else {
+            $this->guard()->login($user);
+            $this->session()->regenerate();
+
+            return redirect(RouteServiceProvider::HOME);
+        }
+    }
+
+    /**
+     * Guard Name
+     *
+     * @param string $guardName
+     * @return
+     */
+    public function guard($guardName = 'web')
+    {
+        return Auth::guard($guardName);
     }
 }
--- src/routes/laravel-2fa.php
+++ src/routes/laravel-2fa.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\TwoFactorAuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +10,9 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => 'guest'], function () {
     Route::get('/login', [LoginController::class, 'index'])->name('login');
     Route::post('/login', [LoginController::class, 'authenticateLogin'])->name('authenticate');
+
+    Route::get('/two-factor-authentication', [TwoFactorAuthController::class, 'index'])->name('two-factor-authentication');
+    Route::post('/two-factor-authentication', [TwoFactorAuthController::class, 'validateTwoFactor'])->name('two-factor-authentication.validate');
 
     Route::get('/register', [RegisterController::class, 'index'])->name('register');
     Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
