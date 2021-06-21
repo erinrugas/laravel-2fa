70bda158a8e0ea5e5dbf6b680f9b67c3866d0a47
Erin Rugas <erinjohnrugas@gmail.com>
2021-06-21 15:52:57 +0800

    added 2fa controller and view

---
+++ src/Http/Controllers/Auth/TwoFactorAuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\ValidateTwoFactorAuth;
+use Illuminate\Http\Request;
+
+class TwoFactorAuthController extends Controller
+{
+    /**
+     * 
+     *
+     * @return void
+     */
+    public function index()
+    {
+        if (session()->has('user_id')) {
+            return view('auth.two-factor-auth');
+        }
+
+        return redirect()->route('login');
+    }
+
+    /**
+     * Validate Two Factor Authentication
+     *
+     * @param ValidateTwoFactorAuth $request
+     * @return void
+     */
+    public function validateTwoFactor(ValidateTwoFactorAuth $request)
+    {
+        if (strpos($request->two_factor_auth, '-') !== false) {
+            return $request->recoveryCode();
+        }
+        
+        return $request->authenticator();
+    }
+    
+}
---
+++ src/resources/views/auth/two-factor-auth.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.auth')
+
+@section('content')
+<div class="container">
+    <div class="row ">
+        <div class="col-md-12">
+            <div class="card shadow p-3">
+                <div class="card-body">
+                    <main class="form-signin">
+                        <form method="POST" id="login-form" action="{{ route('two-factor-authentication.validate') }}">
+                            @csrf
+                            <h3 class="mb-3 fw-normal">Two Factor Authentication</h3>
+                            
+                            <div class="form-floating mb-2">
+                                <input type="text" class="form-control @error('two_factor_auth') is-invalid @enderror" 
+                                    id="two-factor-auth"
+                                    placeholder="Two Factor Authentication" name="two_factor_auth">
+                                <label for="two-factor-auth">Two Factor Authentication</label>
+
+                                @error('two_factor_auth')
+                                <div class="invalid-feedback text-start" id="invalid-feedback-two-factor-auth">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="auth-msg mb-2">
+                                <small>
+                                    <i>
+                                        Enter the code from the authenticator app on your mobile device.
+                                        <br>
+                                        If you've lost your device, you may enter one of your
+                                        recovery codes.
+                                    </i>
+                                </small>
+                            </div>
+                            <button type="submit" class="btn btn-primary w-100 mb-3" id="submit-two-factor">Submit</button>
+                        </form>
+                    </main>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
