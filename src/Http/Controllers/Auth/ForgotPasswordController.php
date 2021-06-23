ae5b91228c8bd48fcb96a308d470182bcc83d9f4
Erin Rugas <erinjohnrugas@gmail.com>
2021-06-23 15:07:14 +0800

    remove unused code

--- src/Http/Controllers/Auth/ForgotPasswordController.php
+++ src/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\ValidateForgotPassword;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
