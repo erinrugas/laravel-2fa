edb552623bc6c517b45948fddc39568d06713033
Erin Rugas <erinjohnrugas@gmail.com>
2021-06-23 22:14:16 +0800

    added constant route name

--- src/Facades/TwoFactorAuth.php
+++ src/Facades/TwoFactorAuth.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Facade;
 class TwoFactorAuth extends Facade
 {
     /**
+     * Two factor route name
+     */
+    const REDIRECT_TWO_FACTOR = 'two-factor-authentication';
+
+    /**
      * Get the registered name of the component.
      *
      * @return string
